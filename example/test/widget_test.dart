ab7eca0c30dd75f5e4339e1c28ebcc4ef6492223
Farhan Sadik Galib <farhansadikgalib@gmail.com>
2025-07-12 13:16:39 +0600

    Add widget test for Pick Image button functionality

---
+++ example/test/widget_test.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:example/main.dart';
+
+void main() {
+  testWidgets('Pick Image button is present and tappable',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+
+    // Verify the Pick Image button exists
+    expect(find.text('Pick Image'), findsOneWidget);
+
+    // Tap the Pick Image button
+    await tester.tap(find.text('Pick Image'));
+    await tester.pump();
+
+    // You can add more expectations here if you mock image picking
+  });
+}
