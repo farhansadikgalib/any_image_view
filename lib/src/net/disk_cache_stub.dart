fef32d101192c2fa8c1a82d7c9f2828de2e97bd6
farhan <farhansadikgalib@gmail.com>
2026-09-19 19:57:20 +0600

    release: v2.5.0

---
+++ lib/src/net/disk_cache_stub.dart
@@ -0,0 +1,7 @@
+import 'dart:typed_data';
+
+Future<Uint8List?> read(String key, Duration maxAge) async => null;
+Future<void> write(String key, Uint8List bytes) async {}
+Future<void> remove(String key) async {}
+Future<void> clear() async {}
+Future<String?> directoryPath() async => null;
